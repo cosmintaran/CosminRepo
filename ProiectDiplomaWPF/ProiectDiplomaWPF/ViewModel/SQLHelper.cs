14f919e3e6d560af876f53ce9cc7a20cb6ea67fb
Taran Constantin <cosmintaran@yahoo.com>
2016-03-13 18:54:15 +0200

    Proiect Diploma

--- ProiectDiplomaWPF/ProiectDiplomaWPF/ViewModel/SQLHelper.cs
+++ ProiectDiplomaWPF/ProiectDiplomaWPF/ViewModel/SQLHelper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 
-namespace ProiectDiplomaWPF.Model
+namespace ProiectDiplomaWPF.ViewModel
 {
     class SQLHelper
     {
