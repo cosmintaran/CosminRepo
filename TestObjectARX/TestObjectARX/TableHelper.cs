47291ee6304712d302bea0a8f34a6084907d36b2
Taran Constantin <cosmintaran@yahoo.com>
2016-03-15 21:51:00 +0200

    Laptop

--- TestObjectARX/TestObjectARX/TableHelper.cs
+++ TestObjectARX/TestObjectARX/TableHelper.cs
@@ -5,6 +5,7 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Colors;
 using Autodesk.AutoCAD.EditorInput;
+using System;
 namespace TestObjectARX
 {
     public class TableHelper
@@ -12,7 +13,7 @@ namespace TestObjectARX
         Point3d insertionPoint;
         Document m_doc;
         Database m_db;
-        const float rowHeigh = 0.1334f;
+        const float m_rowHeigh = 0.1334f;
         public TableHelper(Document doc)
         {
             this.m_doc = doc;
@@ -29,8 +30,106 @@ namespace TestObjectARX
 
         public void DrawCoordTable(Dictionary<DBText, DBPoint> vertexCoords)
         {
-            List<Line3d> tableLines = new List<Line3d>();
+            const int nrColoane = 3;
+            int nrRanduri = vertexCoords.Count+2;
+            bool isLayerExist = false;
+            ObjectId obTs = ObjectId.Null;
+            Color layerColor = Color.FromColorIndex(ColorMethod.ByAci, 7);
+            isLayerExist = LayerHelper.CreateLayer(m_doc, "tabelInventar", layerColor);
+            TextHelper.CreateTextStyle(m_doc, "Arial", "arial.shx");
+            CreateTableStyle("Inventar", "Arial", LineWeight.LineWeight018, out obTs);
 
+            PromptPointResult insertionPoint = m_doc.Editor.GetPoint("\nSelecteaza punctul de insertie al tabelului");
+            if (insertionPoint.Status == PromptStatus.OK)
+            {
+
+                Table invTable = new Table();
+                if (isLayerExist)
+                    invTable.Layer = "tabelInventar";
+
+                if (obTs == ObjectId.Null)
+                    invTable.TableStyle = m_db.Tablestyle;
+                else
+                    invTable.TableStyle = obTs;
+                invTable.NumColumns = nrColoane;
+                invTable.NumRows = nrRanduri;
+                invTable.SetColumnWidth(0, 3);
+                invTable.SetColumnWidth(1, 8);
+                invTable.SetColumnWidth(2, 8);
+                invTable.SetRowHeight(0, 0.9);
+                invTable.SetRowHeight(1, 0.9);
+                invTable.Position = insertionPoint.Value;
+
+                /*********** Tabel Title **************/
+                invTable.SetTextHeight(0, 0, 0.755);
+                invTable.SetTextString(0, 0, "Inventar de coordonate");
+                invTable.SetAlignment(0, 0, CellAlignment.MiddleCenter);
+
+                /*********** Table Header *************/
+                string[] header = { "Nr.\ncrt.", "X[m]", "Y[m]" };
+                for (int i = 0; i < nrColoane; i++)
+                {
+                    invTable.SetTextHeight(1, i, 0.7);
+                    invTable.SetTextString(1, i, header[i].ToString());
+                    invTable.SetAlignment(1, i, CellAlignment.MiddleCenter);
+                }
+                int contor = 2;
+                foreach (var entry in vertexCoords)
+                {
+                   
+                    invTable.SetRowHeight(contor, 0.8);
+
+                    invTable.SetTextHeight(contor, 0, 0.8);
+                    invTable.SetTextHeight(contor, 1, 0.8);
+                    invTable.SetTextHeight(contor, 2, 0.8);
+
+                    invTable.SetTextString(contor, 0, (contor-1).ToString());
+                    invTable.SetTextString(contor, 1, entry.Value.Position.Y.ToString("#.###"));
+                    invTable.SetTextString(contor, 2, entry.Value.Position.X.ToString("#.###"));
+
+                    invTable.SetAlignment(contor, 0, CellAlignment.MiddleCenter);
+                    invTable.SetAlignment(contor, 1, CellAlignment.MiddleCenter);
+                    invTable.SetAlignment(contor, 2, CellAlignment.MiddleCenter);
+                    contor++;
+                }
+
+                using (Transaction tran = m_db.TransactionManager.StartTransaction())
+                {
+                    BlockTable blkTb = tran.GetObject(m_db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                    BlockTableRecord blkTbRec = tran.GetObject(blkTb[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                    blkTbRec.AppendEntity(invTable);
+                    tran.AddNewlyCreatedDBObject(invTable, true);
+
+                    tran.Commit();
+                }
+                using (Transaction tran = m_db.TransactionManager.StartTransaction())
+                {
+                    ObjectId tableID = invTable.ObjectId;
+                    DBObjectCollection entitySet = new DBObjectCollection();
+                    invTable.Explode(entitySet);
+
+                    BlockTableRecord table = (BlockTableRecord)tran.GetObject(m_db.CurrentSpaceId, OpenMode.ForWrite);
+                    foreach (DBObject obj in entitySet)
+                    {
+                        if (obj is Entity)
+                        {
+                            table.AppendEntity((Entity)obj);
+                            tran.AddNewlyCreatedDBObject(obj, true);
+                        }
+                    }
+                    tran.Commit();
+                }
+
+                using (Transaction tran = m_db.TransactionManager.StartTransaction())
+                {
+                    //BlockTable bTB = tran.GetObject(invTable.ObjectId, OpenMode.ForRead) as BlockTable;
+                    BlockTableRecord bTBR = tran.GetObject(invTable.ObjectId, OpenMode.ForWrite) as BlockTableRecord;
+                    invTable.Erase();
+                    //bTBR.Erase(true);
+                    tran.Commit();
+                }
+
+            }
         }
 
         public void DrawSupUtilTabel(List<Incapere> camera, string titluTabel)
@@ -38,12 +137,14 @@ namespace TestObjectARX
             const double inaltimeText = 0.2;
             const int colorIndex = 7; //alb
             const int nrColoane = 3;
-            int nrRanduri = camera.Count() + 4;
+            int nrRanduri = camera.Count() + 2;
             bool existLayer = false;
-            string[] header = { "Nr.crt", "Destinatie incapere", "Supraf.Utila [mp]" };
+            ObjectId obTs = ObjectId.Null;
+            string[] header = { "Nr.\nincapere", "Destinatie incapere", "Supraf.Utila\n [mp]" };
             Color layerColor = Color.FromColorIndex(ColorMethod.ByAci, colorIndex);
             existLayer = LayerHelper.CreateLayer(m_doc, "Plansa", layerColor);
             TextHelper.CreateTextStyle(m_doc, "Arial", "arial.shx");
+            CreateTableStyle("Apartamentare", "Arial", LineWeight.LineWeight018,out obTs);
             PromptPointResult insertionPoint = m_doc.Editor.GetPoint("\nSelecteaza punctul de insertie al tabelului");
             if (insertionPoint.Status == PromptStatus.OK)
             {
@@ -51,38 +152,50 @@ namespace TestObjectARX
                 Table supTable = new Table();
                 if (existLayer)
                     supTable.Layer = "Plansa";
-                supTable.TableStyle = m_db.Tablestyle;
+
+                if (obTs == ObjectId.Null)
+                    supTable.TableStyle = m_db.Tablestyle;
+                else
+                    supTable.TableStyle = obTs;
+
                 supTable.NumColumns = nrColoane;
                 supTable.NumRows = nrRanduri;
-                //supTable.SetColumnWidth(0.6);
-                supTable.SetRowHeight(0.25);
+                supTable.SetColumnWidth(0, 1.92);
+                supTable.SetColumnWidth(1, 5.35);
+                supTable.SetColumnWidth(2, 3.5);
+                supTable.SetRowHeight(0, 0.5);
+                supTable.SetRowHeight(1, 0.9);
                 supTable.Position = insertionPoint.Value;
+
                 /*********** Tabel Title **************/
-                supTable.SetTextHeight(0, 0, 0.22);
+                supTable.SetTextHeight(0, 0, inaltimeText+0.05);
                 supTable.SetTextString(0, 0, titluTabel);
                 supTable.SetAlignment(0, 0, CellAlignment.MiddleCenter);
+
                 /*********** Table Header *************/
                 for (int i = 0; i < nrColoane; i++)
                 {
-                    supTable.SetTextHeight(1, i, 0.2);
+                    supTable.SetTextHeight(1, i, 0.22);
                     supTable.SetTextString(1, i, header[i].ToString());
                     supTable.SetAlignment(1, i, CellAlignment.MiddleCenter);
                 }
 
                 Incapere[] inc = camera.ToArray();
-                for (int i = 2; i < inc.Length; i++)
+                for (int i = 0; i < inc.Length; i++)
                 {
-                    supTable.SetTextHeight(i, 0, 0.2);
-                    supTable.SetTextHeight(i, 1, 0.2);
-                    supTable.SetTextHeight(i, 2, 0.2);
+                    supTable.SetRowHeight(i + 2, 0.35);
 
-                    supTable.SetTextString(i, 0, inc[i].NrIncapere.ToString());
-                    supTable.SetTextString(i, 1, inc[i].DestinatieIncapere.ToString());
-                    supTable.SetTextString(i, 2, inc[i].SuprafataIncapere.ToString());
+                    supTable.SetTextHeight(i+2, 0, 0.2);
+                    supTable.SetTextHeight(i+2, 1, 0.2);
+                    supTable.SetTextHeight(i+2, 2, 0.2);
 
-                    supTable.SetAlignment(i, 0, CellAlignment.MiddleCenter);
-                    supTable.SetAlignment(i, 1, CellAlignment.MiddleCenter);
-                    supTable.SetAlignment(i, 2, CellAlignment.MiddleCenter);
+                    supTable.SetTextString(i+2, 0, inc[i].NrIncapere.ToString());
+                    supTable.SetTextString(i+2, 1, inc[i].DestinatieIncapere.ToString());
+                    supTable.SetTextString(i+2, 2, Math.Round(inc[i].SuprafataIncapere,2).ToString());
+
+                    supTable.SetAlignment(i+2, 0, CellAlignment.MiddleCenter);
+                    supTable.SetAlignment(i+2, 1, CellAlignment.MiddleCenter);
+                    supTable.SetAlignment(i+2, 2, CellAlignment.MiddleCenter);
                 }
                 supTable.GenerateLayout();
 
@@ -99,12 +212,12 @@ namespace TestObjectARX
 
         }
 
-        private void CreateTableStyle(string tableStyleName, string textStyleName, LineWeight lineWeight)
+        private void CreateTableStyle(string tableStyleName, string textStyleName, LineWeight lineWeight, out ObjectId tsObjID)
         {
 
             using (Transaction tr = m_db.TransactionManager.StartTransaction())
             {
-                ObjectId tsObjID = ObjectId.Null;
+                tsObjID = ObjectId.Null;
                 DBDictionary dbDic = tr.GetObject(m_db.TableStyleDictionaryId, OpenMode.ForRead) as DBDictionary;
                 if (dbDic.Contains(tableStyleName))
                 {
@@ -114,9 +227,11 @@ namespace TestObjectARX
                 {
                     TableStyle newTs = new TableStyle();
                     TextStyleTable test = tr.GetObject(m_db.TextStyleTableId, OpenMode.ForRead) as TextStyleTable;
-                    if (test.Has(textStyleName))
-                        newTs.SetTextStyle(m_db.TextStyleTableId, textStyleName);
+                    //if (test.Has(textStyleName))
+                        //newTs.SetTextStyle(m_db.TextStyleTableId, textStyleName);
                     newTs.SetGridLineWeight(lineWeight, (int)GridLineType.AllGridLines, (int)GridLineType.AllGridLines);
+                    tsObjID = newTs.PostTableStyleToDatabase(m_db, tableStyleName);
+                    tr.AddNewlyCreatedDBObject(newTs, true);
                 }
 
                 tr.Commit();
